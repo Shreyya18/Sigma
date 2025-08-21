4134f592f5c20a8dabca851b81a3a01a224a6b52
shreyya18 <shreyapoojary251@gmail.com>
2025-08-21 20:39:34 +0530

    js

---
+++ practice-32/script.js
@@ -0,0 +1,10 @@
+alert("hello world");
+var a=prompt("Enter a number");
+var b=confirm("Do you want to continue?");
+if(b)
+{
+    console.log("You entered: " + a);
+}
+else{
+    console.log("You chose not to continue.");
+}
