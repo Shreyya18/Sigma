0f7a9a2323a7194e69fe9b0584a3a717a0a8a557
shreyya18 <shreyapoojary251@gmail.com>
2025-08-23 19:53:14 +0530

    var and let

--- practice-33/script.js
+++ practice-33/script.js
@@ -1,6 +1,17 @@
 console.log("hello world");
 var a=5;
+let d=20;
+console.log("var variable"+a);
+console.log("let variable"+d);
 var b=2;
 var c="string";
 console.log(a+b);
 console.log(typeof a, typeof b, typeof c);
+{
+    var a=10;
+    console.log("block "+ a);
+    let d=30;
+    console.log("block"+ d);
+}
+console.log("outside block"+a); 
+console.log("outside block"+d);
