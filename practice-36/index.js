67718427df32e5f72127a72950ffd95e1899e121
shreyya18 <shreyapoojary251@gmail.com>
2025-08-24 16:23:41 +0530

    js functions

---
+++ practice-36/index.js
@@ -0,0 +1,15 @@
+console.log("js functions");
+
+function greet(name){
+    console.log("hello",name);
+}
+
+greet("john");
+greet("alice");
+
+const func1=(x)=>{
+    console.log("this is func1",x);
+}
+
+func1(5);
+func1("hello")
