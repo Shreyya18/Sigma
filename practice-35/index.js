9b0c55e40deb7c15023463484f1563847301152d
shreyya18 <shreyapoojary251@gmail.com>
2025-08-24 16:07:06 +0530

    while loop

--- practice-35/index.js
+++ practice-35/index.js
@@ -3,4 +3,29 @@ for(let i=0;i<=10;i++){
     console.log(i);
 }
 
-let 
+let obj={
+    name:"john",
+    age:30,
+    city:"New York"
+}
+for(let key in obj){
+    console.log(key, obj[key]);
+}
+
+for (const element of obj.name) {
+    console.log(element);
+    
+}
+
+let i=0;
+while(i<5){
+    console.log(i);
+    i++;
+}
+ console.log("do while loop");
+let j=10;
+do{
+    console.log(j);
+    j++;
+}
+while(j<5);
