666f42e0fad2f1dba16084d4741a482c3aa992ca
shreyya18 <shreyapoojary251@gmail.com>
2025-08-26 20:04:25 +0530

    string operations

---
+++ practice-37/index.js
@@ -0,0 +1,14 @@
+let a="john"
+console.log("hello", a);
+console.log("hello"+ a);
+console.log(`hello ${a}`);
+console.log(`john's age is 20`);
+console.log("john\'s age is 20");
+
+let b="alice";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(1,3));
+console.log(b.replace("al","s"));
+console.log(b.concat(a));
