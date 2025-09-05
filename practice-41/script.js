d9feb5b16f4b6f9a18768d93347ad25b6170fc74
shreyya18 <shreyapoojary251@gmail.com>
2025-09-05 19:45:45 +0530

    insert and remove elements using js

---
+++ practice-41/script.js
@@ -0,0 +1,17 @@
+document.querySelector(".box");
+document.querySelector(".box").innerText="hyy";
+document.querySelector(".box").innerHTML="hyy";
+document.getElementsByClassName("box")[2].innerText="hyy";
+
+// document.getElementsByClassName("container");
+
+let div=document.createElement("div");
+div.innerHTML="i have been created <b>by shreya</b>";
+div.setAttribute("class", "created");
+document.querySelector(".container").append(div);
+
+let h1=document.createElement("h1");
+h1.innerHTML="good evening";
+h1.setAttribute("id","heading")
+document.querySelector(".container").prepend(h1);
+
