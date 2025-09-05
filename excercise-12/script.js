c1759b4f7379d5de3381c249cb3c9a0dcb664018
shreyya18 <shreyapoojary251@gmail.com>
2025-09-05 18:32:57 +0530

    excercise-12

--- excercise-12/script.js
+++ excercise-12/script.js
@@ -2,5 +2,5 @@ let color=["red","blue","green","yellow","pink","black"];
 
 let arr=document.querySelectorAll(".box") 
 for(let i=0;i<5;i++){
-    arr[i].style.backgroundColor=color[Math.floor(Math.random()*8)];
+    arr[i].style.backgroundColor=color[Math.floor(Math.random()*6)];
 }
