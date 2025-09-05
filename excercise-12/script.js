08c0c7a6e6eb86291c917a89d853cdb547abbcb2
shreyya18 <shreyapoojary251@gmail.com>
2025-09-05 20:58:29 +0530

    random backgroundcolor generator

--- excercise-12/script.js
+++ excercise-12/script.js
@@ -1,6 +1,19 @@
-let color=["red","blue","green","yellow","pink","black"];
+// let color=["red","blue","green","yellow","pink","black"];
 
-let arr=document.querySelectorAll(".box") 
-for(let i=0;i<5;i++){
-    arr[i].style.backgroundColor=color[Math.floor(Math.random()*6)];
+// let arr=document.querySelectorAll(".box") 
+// for(let i=0;i<5;i++){
+//     arr[i].style.backgroundColor=color[Math.floor(Math.random()*6)];
+// }
+
+var boxes=document.querySelectorAll(".box");
+
+for(let i=0;i<boxes.length;i++){
+    box=boxes[i];
+
+    var color="#"+Math.floor(Math.random()*16777215).toString(16);
+
+    var backgroundColor="#"+Math.floor(Math.random()*16777215).toString(16);
+
+    box.style.color=color;
+    box.style.backgroundColor=backgroundColor;
 }
