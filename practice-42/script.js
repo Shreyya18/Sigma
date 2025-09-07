abb1c472ed0da96041333f3c04d992c990042099
shreyya18 <shreyapoojary251@gmail.com>
2025-09-07 20:56:49 +0530

    darkMode

--- practice-42/script.js
+++ practice-42/script.js
@@ -3,3 +3,15 @@ button.addEventListener("click", ()=>{
     let para=document.getElementsByTagName("p")
     para[0].innerText="Hey.. You clicked Me :)";
 })
+
+document.querySelector("button").onclick=function(){
+    document.querySelector(".main").classList.toggle("dark");
+    if( document.querySelector(".main").classList.contains("dark"))
+            document.querySelector("button").innerText="LIGHT MODE";
+    else
+            document.querySelector("button").innerText="DARK MODE";
+
+
+
+}
+
