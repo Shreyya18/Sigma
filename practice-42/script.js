62d28ac181d5c5becad384e5e74bbb10830d303d
shreyya18 <shreyapoojary251@gmail.com>
2025-09-08 10:49:03 +0530

    set interval

--- practice-42/script.js
+++ practice-42/script.js
@@ -1,8 +1,30 @@
 let button=document.getElementById("btn");
-button.addEventListener("click", ()=>{
-    let para=document.getElementsByTagName("p")
-    para[0].innerText="Hey.. You clicked Me :)";
+button.addEventListener("click" ,()=>{
+    // let para=document.getElementsByTagName("p")
+    // para[0].innerText="Hey.. You clicked Me :)";
+    document.querySelector("p").innerText="Hey.. You clicked Me :)"
 })
+window.addEventListener("contextmenu" ,()=>{
+    // let para=document.getElementsByTagName("p")
+    // para[0].innerText="Hey.. You clicked Me :)";
+    alert("do not right click")
+})
+window.addEventListener("keyup" ,()=>{
+    // let para=document.getElementsByTagName("p")
+    // para[0].innerText="Hey.. You clicked Me :)";
+    alert("keyup");
+})
+window.addEventListener("keydown" ,()=>{
+    // let para=document.getElementsByTagName("p")
+    // para[0].innerText="Hey.. You clicked Me :)";
+    alert("keydown");
+})
+window.addEventListener("keypress" ,()=>{
+    // let para=document.getElementsByTagName("p")
+    // para[0].innerText="Hey.. You clicked Me :)";
+    alert("keypress");
+})
+
 
 document.querySelector("button").onclick=function(){
     document.querySelector(".main").classList.toggle("dark");
@@ -14,4 +36,17 @@ document.querySelector("button").onclick=function(){
 
 
 }
+
+function getRandom(){
+    let randColor= "#"+Math.floor(Math.random()*167772155).toString(16);
+    return randColor;
+}
+setInterval(() => {
+    document.querySelector("nav").style.backgroundColor= getRandom();
+}, 100);
+
+setTimeout(() => {
+    document.querySelector("body").style.backgroundColor="blue"
+}, 4000);
+
 
