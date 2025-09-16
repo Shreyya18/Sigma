8d94e5dc4dff70b8e94fd42250444aa4eb17f79f
shreyya18 <shreyapoojary251@gmail.com>
2025-09-16 20:01:22 +0530

    finally exmaple

--- practice-46/script.js
+++ practice-46/script.js
@@ -3,14 +3,32 @@ let b= prompt("enter second number")
 
 let sum= parseInt(a)+parseInt(b)
 console.log(sum)
-
+// throw example
 if(isNaN(a) || isNaN(b))
     throw TypeError ("this is not allowed")
 
-
+// try and catch example
 try {
     console.log(sum*x)
     
 } catch (error) {
-    console.log("error occured" )
+    console.log("error occured for x" )
 }
+
+//finally example
+function main(){
+    try{
+        console.log(sum*y)
+        return true
+    }
+    catch(error){
+        console.log("error occured again for y")
+        return false
+    }
+    finally{
+        console.log('files are being closed...')
+    }
+}
+
+let c= main()
+console.log(c)
