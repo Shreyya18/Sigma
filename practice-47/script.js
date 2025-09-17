90f671edbe918e33c85250f9b8db395573419d5c
shreyya18 <shreyapoojary251@gmail.com>
2025-09-17 22:05:54 +0530

    destructing

--- practice-47/script.js
+++ practice-47/script.js
@@ -13,4 +13,17 @@ async function sleep() {
     console.log(a)
 })()
 
+// destructing using spread operator
+function destruct(){
+    let [a,b]=[1,2]
+    console.log(a)
+    console.log(b)
 
+    let [c,d, ...spread]=[1,4,2,5,8]
+    console.log(c)
+    console.log(d)
+    console.log(spread)
+    console.log(sum(c,d))
+}
+
+destruct()
