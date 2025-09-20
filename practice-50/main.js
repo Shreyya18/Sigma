e9c2b8afb81092778ca63356e4bf8165e2f71470
shreyya18 <shreyapoojary251@gmail.com>
2025-09-20 12:24:34 +0530

    Refactor module imports and exports; add mymodule2.js with exports

--- practice-50/main.js
+++ practice-50/main.js
@@ -1,4 +1,7 @@
-import {a} from "./mymodule1.js";
+// import {a} from "./mymodule1.js";
+// console.log(a);
+// import num from "./mymodule1.js"
+// console.log(num);
+
+const a=require("./mymodule2.js");
 console.log(a);
-import num from "./mymodule1.js"
-console.log(num);
---
+++ practice-50/mymodule2.js
@@ -0,0 +1,4 @@
+module.exports.a=1;
+
+let c=80;
+module.exports=c;
