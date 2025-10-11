93f03c56cb33f4d5ec2446976b33aca51ba8e03f
shreyya18 <shreyapoojary251@gmail.com>
2025-10-11 11:06:45 +0530

    Enhance main.js with improved response messages and add index.html and about.html templates

--- practice-53/main.js
+++ practice-53/main.js
@@ -4,7 +4,7 @@ const port=3000
 
 app.use(express.static('public'))
 app.get('/' , (req,res)=>{
-    res.send("Hello World")
+    res.send("Hello World... Welcome to my first express app")
 }
 )
 
@@ -12,6 +12,17 @@ app.post('/', (req,res)=>{
     res.send("You have sent a post request")
 }
 )
+
+app.get('/index', (req,res)=>{
+    res.sendFile('templates/index.html', {root:__dirname})
+    // res.send(__dirname)
+})
+
+app.get('/about', (req,res)=>{
+    // res.download('templates/about.html', {root:__dirname})
+    res.redirect('https://www.google.com')
+})
+
 app.listen((port), ()=>{
     console.log(`Example app listening on port ${port}`)
 })
