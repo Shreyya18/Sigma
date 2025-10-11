ba1239cff448a2a0144d9f4ac6a886cdcaa62b41
shreyya18 <shreyapoojary251@gmail.com>
2025-10-11 12:41:04 +0530

    Add request logging middleware to main.js

--- practice-54/main.js
+++ practice-54/main.js
@@ -1,7 +1,18 @@
 const express= require('express')
-
+const fs=require('fs')
 const app=express()
 const port=3000
+// Middleware in Express.js are functions that process requests before they reach the final route handler — like checkpoints that can modify, validate, or terminate the request
+
+const myLogger=(req,res, next)=>{
+    console.log("logged")
+    fs.appendFileSync('log.txt', `New request made at ${new Date()}\n`)
+    // res.send(`Request logged at ${new Date()}`)
+    next()
+}
+
+app.use(myLogger)
+
 
 app.get('/', (req,res)=>{
     res.send("Hello World...")
