63592f3c6c36c772045c3949eda76cd85e96340b
shreyya18 <shreyapoojary251@gmail.com>
2025-10-20 11:51:27 +0530

    change reuire to import

--- practice-60/main.js
+++ practice-60/main.js
@@ -1,7 +1,9 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const path = require('path');
-const { Employee } = require('./mongo.js');
+
+ 
+import mongoose from 'mongoose';
+import express from 'express';
+import path from 'path';
+import { Employee } from './mongo.js';
 
 let conn= await mongoose.connect("mongodb://localhost:27017/employee")
 
@@ -14,10 +16,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
-app.get('/index', (req, res) => {
-    res.sendFile('templates/index.html', { root: __dirname })
-    // res.send(__dirname)
-})
+// app.get('/index', (req, res) => {
+//     res.sendFile('templates/index.html', { root: __dirname })
+//     // res.send(__dirname)
+// })
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 })
--- practice-60/mongo.js
+++ practice-60/mongo.js
@@ -1,5 +1,4 @@
-const mongoose=require('mongoose');
-
+import mongoose from 'mongoose';
 const employeeSchema = new mongoose.Schema({
     name: String,
     salary : Number,
