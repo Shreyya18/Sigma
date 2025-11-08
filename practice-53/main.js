7966e541597b5f36d41a8e9c7d7824e6cccab060
shreyya18 <shreyapoojary251@gmail.com>
2025-11-08 18:01:38 +0530

    main file

--- practice-53/main.js
+++ practice-53/main.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const blog=require('./routes/blog')
 const app=express()
-const port=3000
+const port=3001
 
 app.use(express.static('public'))
 app.use('/blog', blog)
