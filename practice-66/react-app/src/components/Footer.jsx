f8be5101e401c40daeee145b16d985c427656258
shreyya18 <shreyapoojary251@gmail.com>
2025-11-14 18:11:34 +0530

    footer

---
+++ practice-66/react-app/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './component.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+       <p>&copy;Copyright</p> 
+       <ul className='footer-ul'>
+        <li><a href="#">LinkedIn</a></li>
+        <li><a href="#">Twitter</a></li>
+        <li><a href="#">Instagram</a></li>
+       </ul>
+    </div>
+  )
+}
+
+export default Footer
