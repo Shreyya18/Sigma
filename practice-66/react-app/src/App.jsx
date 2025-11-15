e7729e090ee4522338fcf3b61cd74aa3708c367f
shreyya18 <shreyapoojary251@gmail.com>
2025-11-15 15:01:46 +0530

    app

--- practice-66/react-app/src/App.jsx
+++ practice-66/react-app/src/App.jsx
@@ -1,11 +1,19 @@
 import NavBar from "./components/NavBar"
 import Footer from "./components/Footer"
+import Card from "./components/Card"
+import './App.css'
+
 function App() {
   
   return (
     <>
      <NavBar/>
+     <div className="cardGrid">
+     <Card title={"HTML"} desc={"hyper text markup language"}/>
+     <Card title={"CSS"} desc={"Cascading Style sheet"}/>
+     <Card title="Javascript" desc="SCripting language"/>
      <Footer/>
+     </div>
     </>
   )
 }
