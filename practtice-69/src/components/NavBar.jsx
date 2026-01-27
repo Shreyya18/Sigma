1b25b1a5816880b25fdf1d89c30d446f4e307f2d
shreyya18 <shreyapoojary251@gmail.com>
2026-01-27 19:48:19 +0530

    add NavBar component with styling

---
+++ practtice-69/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './NavBar.css'
+const NavBar = () => {
+  return (
+    <div>
+        <h1>My Website</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact </li>
+        </ul>
+    </div>
+  )
+}
+
+export default NavBar
