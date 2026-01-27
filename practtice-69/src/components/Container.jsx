5e54f0222078b877de3cc977788c00a8b6f3931b
shreyya18 <shreyapoojary251@gmail.com>
2026-01-27 19:57:44 +0530

    add Container component with styling

--- practtice-69/src/components/Container.jsx
+++ practtice-69/src/components/Container.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import './Container.css'
 const Container = () => {
   return (
-    <div>
+    <div className='container'>
         <h2>Welcome to the Container Component</h2>
     </div>
   )
